f76d0813b2c00a489dec874e68af652285ec771c
WALEED SHEERAZ <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-11-10 15:05:55 +0300

    Update _app.tsx

--- accounts/pages/_app.tsx
+++ accounts/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app'
 import "@/styles/globals.css";
+import Header from '../components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+<Header />
       <Component {...pageProps} />
     </>
   )
