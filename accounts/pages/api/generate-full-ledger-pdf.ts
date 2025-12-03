26c040e73abf43e41f34a121907ee444e3107a55
WALEED SHEERAZ <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-12-03 12:30:19 +0000

    modified:   accounts/pages/api/generate-full-ledger-pdf.ts

--- accounts/pages/api/generate-full-ledger-pdf.ts
+++ accounts/pages/api/generate-full-ledger-pdf.ts
@@ -519,7 +519,7 @@ class FullLedgerPDFGenerator {
           y: tableTop - 34,
           size: 8,
           font: boldFont,
-          color: COLORS.white,
+          color: COLORS.blue800,
         });
         
         xPos += colWidths[colIndex];
